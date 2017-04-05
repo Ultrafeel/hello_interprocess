86cd4cf105f93e37432a4cf4e3430c57d1084a0f
Ultrafeel <miketut@gmail.com>
2017-04-06 00:21:42 +0300

    shared

--- main_b.c
+++ main_b.c
@@ -39,37 +39,89 @@ enum {
 #define ARRAY_SIZE  40000   
 #define MALLOC_SIZE 100000  
 #define SHM_SIZE    100000
-#define SHM_MODE    0600 /* чте­ние и за­пись для вла­дель­ца */
+#define SHM_MODE    0600 /* чтение и запись для владельца */
 
+void err_sys(char const* str)
+{
+	puts(str);
+}
+void err_show(char const* str)
+{     
+    printf("error: %s . erro = %d\n", str, errno);
+}
+void err_showE(char const* str)
+{     
+    printf("error: %s . erro = %d\n", str, errno);
+	exit(1);
+}
+char array[ARRAY_SIZE]; /* неинициализированные данные = bss */
 
-char array[ARRAY_SIZE]; /* не­ини­циа­ли­зи­ро­ван­ные дан­ные = bss */
-
-int main2(void)
+int mainSH(void)
 {
 	int	shmid;
 	char	*ptr, *shmptr;
 	printf("array[] от %p до %p\n", (void *) &array[0], (void*) &array[ARRAY_SIZE]);
 //	678
 //	Глава 15. Межпроцессное взаимодействие
-	printf("стек при­мер­но %p\n", (void *) &shmid);
+	printf("стек примерно %p\n", (void *) &shmid);
 	if ((ptr = malloc(MALLOC_SIZE)) == NULL)
-		err_sys("ошиб­ка вы­зо­ва функ­ции malloc");
-	printf("ди­на­ми­че­ски вы­де­лен­ная об­ласть от %p до %p\n",
+		err_sys("ошибка вызова функции malloc");
+	printf("динамически выделенная область от %p до %p\n",
 		(void *) ptr, (void *) ptr + MALLOC_SIZE);
 	if ((shmid = shmget(IPC_PRIVATE, SHM_SIZE, SHM_MODE)) < 0)
-		err_sys("ошиб­ка вы­зо­ва функ­ции shmget");
-	if ((shmptr = shmat(shmid, 0, 0)) == (void *) - 1)
-		err_sys("ошиб­ка вы­зо­ва функ­ции shmat");
-	printf("сег­мент раз­де­ляе­мой па­мя­ти при­со­еди­нен в ад­ре­сах от %p до %p\n",
+		err_sys("ошибка вызова функции shmget");
+	if ((shmptr = shmat(shmid, 0, 0)) == (void *) -1)
+		err_sys("ошибка вызова функции shmat");
+	printf("сегмент разделяемой памяти присоединен в адресах от %p до %p\n",
 		(void *) shmptr, (void *) shmptr + SHM_SIZE);
 	if (shmctl(shmid, IPC_RMID, 0) < 0)
-		err_sys("ошиб­ка вы­зо­ва функ­ции shmctl");
+		err_sys("ошибка вызова функции shmctl");
 	return (0);
 }
-void err_show(char const* str)
-{     
-    printf("error: %s . erro = %d\n", str, errno);
+typedef unsigned char bool;
+bool const true = 1;
+const bool false = 0;
+
+typedef struct _TShMem {
+	bool flag;
+	long unsigned val;
+} TShMem;
+
+enum {
+	MALLOC_SIZE2 = sizeof(TShMem)
+};
+int shmid = 0;
+	 TShMem	* ptrShMem= 0;	
+	//char *;ptrShMem
+void delSHM()
+{
+	if (0 != shmid) {
+
+		if (shmctl(shmid, IPC_RMID, 0) < 0)
+			err_sys("ошибка вызова функции shmctl");
+		else
+			shmid = 0;
+	}
 }
+
+int mainSH2(void)
+{
+
+
+	errno = 0;
+	if ((shmid = shmget(IPC_PRIVATE, MALLOC_SIZE2, IPC_CREAT|IPC_EXCL|0666)) < 0)
+		err_showE("ошибка вызова функции shmget");
+	errno = 0;
+	if ((ptrShMem = shmat(shmid, 0, 0)) == (void *) - 1)
+		err_showE("ошибка вызова функции shmat");
+	printf("сегмент разделяемой памяти присоединен в адресах от %p до %p\n",
+		(void *) ptrShMem, (void *) ptrShMem + MALLOC_SIZE2);
+	ptrShMem->flag = false;
+//	EACCES		13	/* Permission denied */
+	return(0);
+}
+
+
 
 int main(int argc, char **argv)
 {
@@ -79,41 +131,60 @@ int main(int argc, char **argv)
         printf(" Hello, arg = %s\n", argv[0]);
 
     }
-    pid_t pid = 0l
+	mainSH2();
+    pid_t pid = 0;
     if ((pid = fork()) < 0) {
         err_show("fork");
-        return (NULL);
-        /* зна­че­ние errno бу­дет ус­та­нов­ле­но функ­ци­ей fork() */
-    } else if (pid == 0) { /* до­чер­ний про­цесс */
-
+        return (1);
+        /* значение errno будет установлено функцией fork() */
+    } else if (pid == 0) { /* дочерний процесс */
+		puts("proc C started!");
+		
+		do
+		{
+			if (ptrShMem->flag) {
+				printf("value = %ld\n", ptrShMem->val);
+				ptrShMem->flag = false;
+				fflush(stdout);
+			}
+			else
+				sleep(1);
+		} while (1);
 //        key_t ftok(const char *path, int id);
-//        Ес­ли нуж­но соз­дать но­вую струк­ту­ру IPC, а не по­лу­чить ссыл­ку на су­ще­ст­
-//вую­щую, сле­ду­ет в  ар­гу­мен­те flag вме­сте с  фла­гом IPC_CREAT ука­зать флаг
-//IPC_EXCL. В ре­зуль­та­те, ес­ли дан­ная струк­ту­ра IPC уже су­ще­ст­ву­ет, функ­ция
-//вер­нет при­знак ошиб­ки с ко­дом EEXIST.
+//        Если нужно создать новую структуру IPC, а не получить ссылку на сущест
+//вующую, следует в  аргументе flag вместе с  флагом IPC_CREAT указать флаг
+//IPC_EXCL. В результате, если данная структура IPC уже существует, функция
+//вернет признак ошибки с кодом EEXIST.
         exit(0);
     }   
-    
+    atexit(delSHM);
+
     setbuf(stdin, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
     int i;
     char line[MAXLINE] = "";
     int n = -1;
-    char * pgs = 0;
-    do {
-        // n = read(STDIN_FILENO, &i, sizeof(i));
-        printf(__FILE__" op N: %d\n", n);
-      pgs = fgets(line, MAXLINE, stdin);
-        if (pgs != 0) {
-            printf(" line recieved: '%s'\n", pgs);
-            i = atoi(pgs);
+	char * pgs = 0;
+	do {
+		// n = read(STDIN_FILENO, &i, sizeof(i));
+		printf(__FILE__" op N: %d\n", n);
+		errno = 0;
+		pgs = fgets(line, MAXLINE, stdin);
+		if (pgs != 0) {
+			printf(" line recieved: '%s'\n", pgs);
+			i = atoi(pgs);
             printf(" num recieved: %d\n", i);
             unsigned long int isqr = i*i;
-            printf(" num square: %ul\n", isqr);
-            
+            printf(" num square: %lu\n", isqr);
+			if (ptrShMem->flag)
+				printf("sh mem is busy! \n");
+            ptrShMem->flag = true;
+			ptrShMem->val = isqr;
             //vscanf("%d", &i);
-        } else
+        } else if (errno != 0)
             err_show(" fgets");
-    } while ((pgs != 0)&& (++n < 5));
+		++n
+    } while (1);//((pgs != 0)&& ( < 5));
     //write(STDOUT_FILENO, line, n); 
     //int i = 0;
     //scanf("%d", &i);
